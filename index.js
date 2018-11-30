b508cc80d105e6021df73b7a9d450a45a38c9160
“CatalinaCampos” <catalina.camposcaceres9@gmail.com>
2018-11-30 10:58:16 -0300

    Inicio de uso de position, abortada la misión de flexbox

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-/* Acá va tu código */
+window.onload =() => {
+    // codificar
+    const button = document.getElementById('encode');
+    buttonStart.addEventListener('click', ()=> {
+        let menssage = document.getElementById('encrypt').value;
+        let displacement = document.getElementById('displacement').value;
+        document.getElementById("descrypt").value= window.cipher.encode(displacement, menssage);
+        return window.cipher.encode(displacement, menssage);
+
+
+
+    })
+
+    //decodificar
+    const button2 = document.getElementById('decode');
+    buttonStart2.addEventListener('click', ()=> {
+        let menssage = document.getElementById('descrypt').value;
+        let displacement = document.getElementById('displacement').value;
+        document.getElementById("encrypt").value= window.cipher.decode(displacement, menssage); // si se pone innerHTML en vez de value, no se puede apretar 2 veces el boton cifrar/descifrar porque no pescaaaaa
+        return window.cipher.decode(displacement, menssage);
+
+
+
+    })
+
+}
